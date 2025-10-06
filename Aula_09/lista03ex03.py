6167786384996162f5edd720eba36c00e87c5f3c
Gilbert-Silva <gilbertif1995@outlook.com>
2025-10-06 13:27:52 +0000

    auto associação

---
+++ Aula_09/lista03ex03.py
@@ -0,0 +1,30 @@
+class Conversor:
+    def __init__(self, num):
+        self.set_num(num)
+    def set_num(self, num):
+        self.__num = num
+    def get_num(self):
+        return self.__num
+    def binario(self):
+        restos = []              # pilha
+        n = self.__num
+        while n > 0:
+            restos.append(n % 2) # push - coloca na pilha
+            n = n // 2
+        bin = ""
+        while len(restos) > 0:
+            bin = bin + str(restos.pop()) # pop - retira da pilha
+        return bin
+    def binario2(self):
+        bin = ""
+        n = self.__num
+        while n > 0:
+            bin = str(n % 2) + bin 
+            n = n // 2            
+        return bin
+    def __str__(self):
+        return f"Decimal = {self.__num}, Binário = {self.binario()} {self.binario2()}"
+
+x = Conversor(14)
+print(x)
+
